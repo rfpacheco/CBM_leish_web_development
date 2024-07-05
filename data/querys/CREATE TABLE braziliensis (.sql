3446319a9ea8d2803528e58dd643c52af71a99ee
viskuit <ronnyfph@gmail.com>
2024-07-05 17:52:54 +0200

    Created all the tables needed

--- data/querys/CREATE TABLE braziliensis (.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE braziliensis (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    Strand VARCHAR(255) DEFAULT NULL,
-    ortholog VARCHAR(255) DEFAULT NULL,
-    associatedfunction VARCHAR(255) DEFAULT NULL
-);
