503a9338d523911b7489e416a588720b3dc4792e
ronnyfph <ronnyfph@gmail.com>
2024-07-08 16:37:23 +0200

    Rewrote json data for infantum donwloads

---
+++ data/sql/querys/CREATE_TABLES.sql
@@ -0,0 +1,30 @@
+-- CREATE TABLE infantum (
+--     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     cdsid VARCHAR(255) DEFAULT NULL,
+--     ortholog VARCHAR(255) DEFAULT NULL,
+--     associatedfunction VARCHAR(255) DEFAULT NULL
+-- );
+
+-- CREATE TABLE donovani (
+--     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     cdsid VARCHAR(255) DEFAULT NULL,
+--     ortholog VARCHAR(255) DEFAULT NULL,
+--     associatedfunction VARCHAR(255) DEFAULT NULL
+-- );
+
+-- CREATE TABLE braziliensis (
+--     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     cdsid VARCHAR(255) DEFAULT NULL,
+--     ortholog VARCHAR(255) DEFAULT NULL,
+--     associatedfunction VARCHAR(255) DEFAULT NULL
+-- );
+
+-- CREATE TABLE major (
+--     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     Strand ENUM('+', '-', '.') NOT NULL,
+--     TranscriptLocation VARCHAR(255) DEFAULT NULL,
+--     cdsid VARCHAR(255) DEFAULT NULL,
+--     cdsLocation VARCHAR(255) DEFAULT NULL,
+--     AdditionalInformation TEXT DEFAULT NULL,
+--     AssociatedFunction TEXT DEFAULT NULL
+-- )
