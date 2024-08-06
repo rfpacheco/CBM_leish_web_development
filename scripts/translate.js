0165c36a16dc932916cce6d0085b5e125b6a8a67
ronnyfph <ronnyfph@gmail.com>
2024-08-06 10:44:42 +0200

    Finished the aesthetics of the button exactly how I wanted it

--- scripts/translate.js
+++ scripts/translate.js
@@ -1,11 +1,21 @@
 function changeLanguage(language) {
     fetch(`./data/lang/${language}.json`)
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
         .then(data => {
             document.querySelector('#lang_genome_decoding').innerText = data.genome_decoding;
             document.querySelector('#lang_about_us').innerText = data.about_us;
             document.querySelector('#lang_species').innerText = data.species;
             document.querySelector('#lang_downloads').innerText = data.downloads;
+
+            // Update the flag image based on selected option
+            const selectedOption = document.querySelector(`#language-dropdown option[value="${language}"]`);
+            const flagSrc = selectedOption.getAttribute('data-flag');
+            document.querySelector('#selected-flag').src = flagSrc;
         })
         .catch(error => console.error('Error fetching language file:', error));
 }
