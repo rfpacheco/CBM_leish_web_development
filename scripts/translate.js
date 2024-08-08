d80c71a66625e519939a3cec0b02280ed3b2b478
ronnyfph <ronnyfph@gmail.com>
2024-08-08 12:58:21 +0200

    Removed redundancty from translate.js

--- scripts/translate.js
+++ scripts/translate.js
@@ -8,57 +8,8 @@ document.addEventListener('DOMContentLoaded', () => {
         loadLanguage(lang);
     };
 
+    // Expose changeLanguage globally if needed
     window.changeLanguage = changeLanguage;
-
-    // Function to load language
-    const loadLanguage = (lang) => {
-        fetch(`./data/lang/${lang}.json`)
-            .then(response => response.json())
-            .then(data => {
-                document.getElementById('lang_genome_decoding').textContent = data.genome_decoding;
-                document.getElementById('lang_about_us').textContent = data.aboutUs;
-                document.getElementById('lang_mission').textContent = data.mission;
-                document.getElementById('lang_team').textContent = data.team;
-                document.getElementById('lang_species').textContent = data.species;
-                document.getElementById('lang_downloads').textContent = data.downloads;
-                document.getElementById('lang_reports').textContent = data.reports;
-                document.getElementById('lang_datasets').textContent = data.datasets;
-                document.getElementById('lang_software').textContent = data.software;
-                document.getElementById('team-frontshow-text-header').querySelector('h3').textContent = data.theTeam;
-                document.getElementById('team-frontshow-text').querySelector('p').textContent = data.theTeam_description;
-                document.getElementById('team-frontshow-button').textContent = data.theTeam_learnMore;
-
-                // Load news content
-                loadNews(lang);
-            })
-            .catch(error => console.error('Error loading language:', error));
-        
-        // Change flag
-        const flagSrc = languageDropdown.querySelector(`option[value="${lang}"]`).dataset.flag;
-        selectedFlag.src = flagSrc;
-    };
-
-    // Load language from localStorage if exists
-    const savedLanguage = localStorage.getItem('selectedLanguage') || 'eng';
-    languageDropdown.value = savedLanguage;
-    loadLanguage(savedLanguage);
-
-    // Event listener for language change
-    languageDropdown.addEventListener('change', (event) => {
-        changeLanguage(event.target.value);
-    });
-});
-
-
-document.addEventListener('DOMContentLoaded', () => {
-    const languageDropdown = document.getElementById('language-dropdown');
-    const selectedFlag = document.getElementById('selected-flag');
-
-    // Function to change language
-    const changeLanguage = (lang) => {
-        localStorage.setItem('selectedLanguage', lang);
-        loadLanguage(lang);
-    };
 
     // Function to load language
     const loadLanguage = (lang) => {
